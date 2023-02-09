2f2b75e45ba7b00674df7c17437045c9da4ff27f
iiamthestorm <reimaul2801@gmail.com>
2023-02-09 20:31:44 +0700

    Add answer to repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+  let result = [];
 
+  if (number == 0) {
+    result = '';
+  } else if (number < 0) {
+    result = 'ERROR';
+  } else if (word == '') {
+    result = '';
+  } else {
+    for (i = 0; i < number; i++) {
+      result += word;
+    }
+  }
+
+	return result;
 };
 
 // Do not edit below this line
