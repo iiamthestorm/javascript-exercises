fa1e4e2fb2232d197ae4d54e99614c3620223268
iiamthestorm <reimaul2801@gmail.com>
2023-02-09 20:56:49 +0700

    add answer to reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
+	let result = [];
+	
+	if (word == '') {
+		return result = '';
+	} else {
+		for (i = 1; i <= word.length; i++) {
+			result = result + word.substr(i - (i + i) ,1);
+		}
+	}
 
+	return result;
 };
 
 // Do not edit below this line
