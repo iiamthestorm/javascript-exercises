6c3dd4642f47cfc1516f8bc0c1de484b19278580
iiamthestorm <reimaul2801@gmail.com>
2023-02-10 11:39:37 +0700

    add answer to removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(words, trash) {
+  args = Array.from(arguments);
+  trash = args.slice(1, args.length + 1);
+  let result = words.filter((words) => !trash.includes(words));
+  return result;
 };
 
 // Do not edit below this line
