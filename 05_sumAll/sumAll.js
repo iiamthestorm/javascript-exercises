73a295d030672dd4a73dc22f848e0d61001c56db
iiamthestorm <reimaul2801@gmail.com>
2023-02-10 12:10:10 +0700

    add answer to sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  let result = 0;
+  if (start < 0 || end < 0) {
+      result = "ERROR";
+  } else if (start === String(start) || end === String(end)) {
+      result = "ERROR";
+  } else if (typeof start === typeof [] || typeof end === typeof []) {
+      result = "ERROR";
+  } else {
+      if (start > end) {
+          for (i = end; i <= start; i++) {
+              result = result + i;
+          }
+      } else if (start < end) {
+          for (i = start; i <= end; i++) {
+              result = result + i;
+          }
+      }
+  }
+  return result;
 };
 
 // Do not edit below this line
